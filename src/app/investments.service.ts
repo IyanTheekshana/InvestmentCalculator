f2a14ad9d76aa78433348fa8314125616b30618e
Iyan Theekshana <iyan.dev123@gmail.com>
2024-07-03 21:16:26 +0200

    added investement service

---
+++ src/app/investments.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Data } from '../data.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InvestmentService {
+  calculateInvestmentResults(data: Data) {
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear =
+        investmentValue * (data.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + data.annualInvestment;
+      const totalInterest =
+        investmentValue - data.annualInvestment * year - data.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested:
+          data.initialInvestment + data.annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investments.service';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  initialInvestment: string = '';
+  annualInvestment: string = '';
+  expectedReturn: string = '';
+  duration: string = '';
 
+  constructor(private instService: InvestmentService) {}
+
+  onSubmit() {
+    let res = this.instService.calculateInvestmentResults({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration: +this.duration,
+    });
+
+    console.log(res);
+  }
 }
