819ddd49dbc71821a08aef35a6f73560591fd43a
Iyan Theekshana <iyan.dev123@gmail.com>
2024-07-03 20:45:41 +0200

    added user-input component with form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserInputComponent],
 })
 export class AppComponent {
   title = 'Investment Calculator';
